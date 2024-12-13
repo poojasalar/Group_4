9665eea19770c9d1326c4f672dccb5d8e0fae8ed
Emmanuel Oyenuga <emmaoyenuga@gmail.com>
2024-12-13 14:31:52 -0600

    feature1 color

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Emmanuel Oyenuga</Text>
+      <Text style={styles.text}>Emmanuel Oyenuga</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +13,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#BA5C12',
+    backgroundColor: '#261132',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    color: '#fff',
+  }
 });
